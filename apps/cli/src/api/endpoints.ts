b6eb6b25a55709148f96ff1bab8dbe4e1d3a0ab4
Matthew Polak <me@matteopolak.com>
2026-05-20 09:45:15 -0700

    chore(cli): remove old typescript cli

--- apps/cli/src/api/endpoints.ts
+++
@@ -1,6 +0,0 @@
-const BASE_PSC = "https://www.uocampus.uottawa.ca/psc/csprpr9www/EMPLOYEE/SA/c/";
-
-export const ENDPOINTS = {
-  termList: `${BASE_PSC}SA_LEARNER_SERVICES.SSR_SSENRL_LIST.GBL`,
-  enrollCart: `${BASE_PSC}SA_LEARNER_SERVICES_2.SSR_SSENRL_CART.GBL`,
-} as const;
